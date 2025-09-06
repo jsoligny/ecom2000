a5bef0889e877d4adb42fb603ad1614cc0f4a30f
jsoligny <jeremie.soligny@gmail.com>
2025-09-06 21:43:06 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,6 +1,6 @@
 # handler.py
 import base64
-from server import (
+from server-gpu import (
     initialize_for_worker, process_bytes_to_dict,
     DEFAULT_MODEL, TARGET_FILL_MIN, TARGET_FILL_MAX,
     AM_FG_T, AM_BG_T, AM_ERODE_PX
